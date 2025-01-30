7f72f9421a40f331f048527db0f9db24f1e78834
Joao Pedro <jpltgamer@gmail.com>
2025-01-30 01:05:33 -0300

    Otp

---
+++ functions/index.js
@@ -0,0 +1,71 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const emailValidator = require("email-validator");
+const htmlBuilder = require("./htmlBuilder");
+
+const app = express();
+
+const sender = {
+    email: '',
+    pass: ''
+}
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: sender.email,
+        pass: sender.pass
+    }
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const { recipient, code } = req.body;
+
+        if (!recipient || !code) {
+            let message = "Recipient and code are required";
+
+            logger.error(message);
+            return res.status(400).json({ error: message });
+        }
+
+        if (!emailValidator.validate(recipient)) {
+            let message = "Invalid email format";
+
+            logger.error(message);
+            return res.status(400).json({ error: message });
+        }
+
+        if (typeof code !== 'string' || code.trim() === '') {
+            let message = "Invalid code format";
+
+            logger.error(message);
+            return res.status(400).json({ error: message });
+        }
+
+        const plainText = `Seu código é: ${code}`;
+
+        const mailOptions = {
+            from: sender.email,
+            to: recipient,
+            subject: `Código de validação de email: ${code}`,
+            text: plainText,
+            html: htmlBuilder(code),
+        };
+
+        await transporter.sendMail(mailOptions);
+
+
+        res.json({ message: "Ok" });
+    } catch (error) {
+        let message = "Error sending email:" + error.message;
+
+        console.error(message);
+        res.status(500).json({ error: message });
+    }
+});
+
+exports.widgets = onRequest(app);
